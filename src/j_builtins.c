85d6afbc8916cf8a756ae9e6b0438906db91c7b1
Jihyeong Yoo <jiyoo@c2r9s3.42seoul.kr>
2022-04-28 17:19:08 +0900

    backup

--- src/j_builtins.c
+++ src/j_builtins.c
@@ -6,7 +6,7 @@
 /*   By: jiyoo <jiyoo@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/18 13:25:13 by jiyoo             #+#    #+#             */
-/*   Updated: 2022/04/27 15:04:02 by jiyoo            ###   ########.fr       */
+/*   Updated: 2022/04/28 16:11:57 by jiyoo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,31 @@
 /*
 
 <서브젝트 요구사항>
-0. 새로운 커맨드 기다릴 때 프롬프트 나타나도록 할 것
+yusong
+0. 새로운 커맨드 기다릴 때 프롬프트 나타나도록 할 것 
 	-> history 구현
 	-> ', " inhibit. 따옴표로 감싸면 String으로 받는 것 구현(단, ""안에 $는 처리 필요)
 	-> 환경변수가 $뒤에 쓰이면 그 값으로 대치되는 것 구현
 1. pipe |
-	->redirecetions <, >, <<, >> 구현
-	-> executable을 search해서 launch하는 것 구현(환경변수를 쓰던, 절대 또는 상대경로를 쓰던)
+	-> redirections <, >, <<, >> 구현
+	-> executable을 search해서 launch하는 것 구현 (환경변수를 쓰던, 절대 또는 상대경로를 쓰던)
 2. $? should expands to the exit status of the most recently executed foreground pipeline.
+
+
+jiyoo
 3. builtin 7개 구현
+
+
 4. ctrl-C ctrl-D ctrl-\ should work like in bash.
+
 
 5. 기타 조건들
 	-> 닫히지 않은 따옴표나 \ 또는 ;는 해석하지 말 것
 	-> 전역변수는 1개를 넘게 사용하지 말 것. 그리고 그 이유에 대해서 생각해 둘 것
 
-
 <구현에 요구되는 자료구조>
-- 커맨드 history 저장을 위한 문자열 리스트
+- 커맨드 history 저장을 위한 문자열 배열. LIFO 형태와 유사하게끔 구현되어야 함
+- 환경변수의 저장을 위한 array_list
 
 
 <할 일>
@@ -43,11 +50,14 @@
 
 이미 구현한 libft, arraylist와 builtin 등등 모든 것들에서 에러처리 컨벤션에 맞게 추가적으로 구현해야 함.
 
-<전달해야 할 것들>
-unset이랑 remove 구현
-	-> 중간을 비우게 되면 문제가 생기고 기본 쉘의 동작과도 결과값이 다르게 나오게 됨
-슬랙에서 논의한대로 add 논리 수정
-	-> 얘도 마찬가지
+<할 것들>
+remove를 하면 해당 index 원소를 그냥 free하고 NULL로 초기화하여 비워두는 것으로 구현
+이후 add로 추가할 때는 위에서부터 빈자리 탐색해서 넣는 걸로 수정
+export에서 그냥 환경변수건 뭐건 뭐가 들어와도 값은 그냥 일단 다 바꿔주는걸로 하자
+yusong님이 구현하신 builtin들 살펴보고 수정할 부분 있으면 수정. 단, echo는 옵션도 있고 하니까 나중에 수정하는 걸로!
+
+위에꺼 다하면 yusong님이 구현중인 부분 같이 살펴보면서 더 잘게잘게 모듈로 나눠서 각자 구현하던지 하자
+
 
 */
 
@@ -122,22 +132,3 @@ int	main(int argc, char **argv, char **envp)
 	printf("\n\n%d %s\n", argc, argv[0]);
 	return (0);
 }
-
-/*
-int main(void)
-{
-    char *argv[] = { "/bin/zsh", "-c", "echo $USER", 0 };
-    char *envp[] =
-    {
-        "HOME=/",
-        "PATH=/bin:/usr/bin",
-        "TZ=UTC0",
-        "USER=beelzebub",
-        "LOGNAME=tarzan",
-        0
-    };
-    execve(argv[0], &argv[0], envp);
-    fprintf(stderr, "Oops!\n");
-    return -1;
-}
-*/
